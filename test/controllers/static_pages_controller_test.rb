edf9ff3c7d911430b5167a7e89227067eb339c1b
jhkiehna <jhkiehna@gmail.com>
2019-05-01 14:42:19 -0400

    cleaned up tests a little

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,33 +1,29 @@
 require 'test_helper'
-include ApplicationHelper
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @base_title = full_title
-  end
 
   test "should get root" do
     get root_url
     assert_response :success
-    assert_select "title", @base_title
+    assert_select "title", full_title
   end
 
   test "should get help" do
     get help_url
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", full_title("Help")
   end
 
   test "should get about" do
     get about_url
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", full_title("About")
   end
 
   test "should get contact" do
     get contact_url
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", full_title("Contact")
   end
 
 end
