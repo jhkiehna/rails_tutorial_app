faaa019d6fea64fb9428fb99bb594110bcf56805
jhkiehna <jhkiehna@gmail.com>
2019-05-06 13:34:22 -0400

    created a migration for the remember token, cleaned up User model according to Rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,26 +3,20 @@ class User < ApplicationRecord
 
   before_save { email.downcase! }
 
-  validates :name,
-    presence: true,
-    length: { maximum: 50 }
-  
-  validates :email,
-    presence: true,
-    length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX },
-    uniqueness: { case_sensitive: false }
-  
-  validates :password,
-    presence: true,
-    length: { minimum: 6 }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
 
   # Returns the hash digest of the given string.
-  def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                  BCrypt::Engine.cost
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Returns a random token.
+  def self.new_token
+    SecureRandom.urlsafe_base64
   end
 end
